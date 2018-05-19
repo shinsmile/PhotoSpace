b9e2cb76ba3a7bf19903f60b9fa1a5a8cf6d8b01
terakenya <acmacm10@icloud.com>
2018-05-19 15:36:28 +0900

    initial_commit2

--- protospace_in_dev-master/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'prototypes#index'
-
-  resources :prototypes, only: [:index, :new, :create, :show]
-  resources :users, only: [:show, :edit, :update]
-end
