775cca8f16eb5606d6434b426bd6f39659af9f0f
ShinsukeArai <shinsuke.r.arai@gmail.com>
2018-06-07 11:30:33 +0900

    コメント表示機能実装(JS)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,10 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.create(content: comment_params[:content], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
     @comments = Comment.where(prototype_id: params[:prototype_id])
-    redirect_to :back
+    if @comment.save
+      render :comment
+    end
+    # redirect_to :back
   end
 
   def comment_params
